365a3fd1a0a23fb805f1b3fdb51301ff5537c96a
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-15 19:43:16 +0100

    Decided to not use Lombok

---
+++ WebPortal/src/main/java/com/example/WebPortal/user/Role.java
@@ -0,0 +1,5 @@
+package com.example.WebPortal.user;
+
+public enum Role {
+
+}
--- WebPortal/src/main/java/com/example/WebPortal/user/User.java
+++ WebPortal/src/main/java/com/example/WebPortal/user/User.java
@@ -1,25 +1,103 @@
 package com.example.WebPortal.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+import java.util.Collection;
+import java.util.List;
+
+
+
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User() {
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public User(Integer id, String username, String email, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
     private String username;
     private String email;
     private String password;
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
