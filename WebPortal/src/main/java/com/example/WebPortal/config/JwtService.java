db1c9867c256ae196f14944b1d9ef725b14c166a
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-15 19:43:40 +0100

    Started working on JwtAuthenticationFilter

---
+++ WebPortal/src/main/java/com/example/WebPortal/config/JwtService.java
@@ -0,0 +1,17 @@
+package com.example.WebPortal.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return null;
+//        return Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(token);
+    }
+}
