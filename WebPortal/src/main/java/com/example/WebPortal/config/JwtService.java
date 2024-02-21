4713ffa16cf41ed092c36f6800f7bcfa65993291
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-21 11:32:21 +0100

    Implemented getSignInKey method

--- WebPortal/src/main/java/com/example/WebPortal/config/JwtService.java
+++ WebPortal/src/main/java/com/example/WebPortal/config/JwtService.java
@@ -2,20 +2,33 @@ package com.example.WebPortal.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "331c3f48294dbbdbe4ccddb77b540f8f9ac653c0258753a84c0977f1e376337e68a7cfbe78cce0c491268f3ed7ee2d85fefadd4d5ff50e2c7be1fcf1666bc7f2";
     public String extractUsername(String token) {
         return null;
     }
 
     private Claims extractAllClaims(String token) {
-        return null;
-//        return Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(token);
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
-    private byte[] getSigninKey() {
-        return null;
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
