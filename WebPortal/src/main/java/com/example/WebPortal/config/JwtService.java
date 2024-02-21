259fc97fcb2a2cbb388c3f2e4e930f5344d5a24b
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-21 11:42:38 +0100

    Implemented generateToken and extractClaim methods

--- WebPortal/src/main/java/com/example/WebPortal/config/JwtService.java
+++ WebPortal/src/main/java/com/example/WebPortal/config/JwtService.java
@@ -2,21 +2,41 @@ package com.example.WebPortal.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "331c3f48294dbbdbe4ccddb77b540f8f9ac653c0258753a84c0977f1e376337e68a7cfbe78cce0c491268f3ed7ee2d85fefadd4d5ff50e2c7be1fcf1666bc7f2";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 + 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parser()
